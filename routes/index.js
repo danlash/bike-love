a7caf5596a1e34203c39a6bd3cd25a2ebc6de36c
Dan Lash <danlash@gmail.com>
2015-01-04 18:38:29 -0500

    layout and pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'Express' });
+});
 module.exports = router;
