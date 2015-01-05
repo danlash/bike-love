3644de4e4c115d51b6abba545c94296cca7017c9
Dan Lash <danlash@gmail.com>
2015-01-04 19:31:00 -0500

    personal details questionaire and questions example design

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var data = { title: 'Valentines 2015', intro: 'Atlanta is awesome.' };
+  res.render('index', data);
 });
 
 router.get('/about', function(req, res) {
