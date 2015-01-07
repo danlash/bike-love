dc8f52ffad9ae1a0188f4763092fa3a686bb6717
Dan Lash <danlash@gmail.com>
2015-01-07 01:03:45 -0500

    load survey from databsae, display questions

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var survey = require('../lib/survey');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  var data = { title: 'Valentines 2015', intro: 'Atlanta is awesome.' };
-  res.render('index', data);
+router.get('/', function(req, res, next) {
+  survey.load(1, function(err, survey){
+    if (err) { return next(err); }
+
+    survey.questions.forEach(function(question){
+      question.multiplechoice = question.type === 'multiplechoice';
+      question.freeform = question.type === 'freeform';
+    });
+
+    res.render('index', survey);  
+  });
+  
 });
 
 module.exports = router;
